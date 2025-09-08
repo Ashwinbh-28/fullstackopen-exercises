cac937dafde6a6ff227d85ad3de92156a4ed9c1c
Ashwin <bhanageashwin28@gmail.com>
2025-09-08 19:46:00 +0530

    Exercise 1.12 anecdotes

--- part1/courseDemo/react-concepts/src/App.jsx
+++ part1/courseDemo/react-concepts/src/App.jsx
@@ -1,58 +1,28 @@
-import { useState } from "react"
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = (props) => {
-  console.log('props value is - ', props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [selected, setSelected] = useState(0)
 
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
+  const handleNext = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
   }
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='left' />
-      {right}
-      <History allClicks={allClicks} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleNext}>Next anecdote</button>
     </div>
   )
 }
